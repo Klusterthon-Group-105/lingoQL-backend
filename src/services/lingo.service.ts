029d6bb6d7259610cdcda4e3d8dc2d0cf26f8485
TijanAyo <ayomidetijani6@gmail.com>
2023-11-26 09:12:15 +0100

    Clean up: Removing unused code snippets

--- src/services/lingo.service.ts
+++ src/services/lingo.service.ts
@@ -3,12 +3,9 @@ import * as net from 'net';
 import * as url from 'url';
 import { PromptTemplate } from "langchain/prompts";
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { CSVLoader } from "langchain/document_loaders/fs/csv";
 import { SqlDatabase } from "langchain/sql_db";
 import { DataSource } from "typeorm";
 import { SqlDatabaseChain } from "langchain/chains/sql_db";
-import { createSqlAgent, SqlToolkit } from "langchain/agents/toolkits/sql";
-
 
 dotenv.config();
 const OpenAIKey = process.env.OPENAI_API_KEY;
@@ -17,32 +14,11 @@ interface convoPayload {
     message: string;
 }
 
-interface connectDBPayload {
-    dbType: any,
-    dbURI: string,
-}
-
 interface askYourDBPayload {
     dbType: any,
     dbURI: string,
     userInput: string
 }
-
-const template = `
-I want you to act as an informant who gives back information about a query from a SQL database
-
-when provided with the query question and the results from the database, answer the query question in a easy-to-understand manner using few word.
-
---Examples--
-reply this query 'how many cars were sold' using this result (11914,)  ->  '11914 cars were sold.'
-reply this query 'what are the different Market Categories return the top 2' using this result [('high-performance',), ('luxury',)]  ->  'The top two market categories are high-performance and luxury.'
-reply this query 'what is the average Number of Doors' using this result [(3.4360933825999327,)] -> The average number of doors is 3.44.
-
-reply this query {query} using this result {result}
-
-response ""
-
-`
 
 class LingoService {
 
@@ -121,40 +97,10 @@ class LingoService {
             } else {
                 throw new Error('Database connection not established.');
             }
-    
-            /* const toolkit = new SqlToolkit(db, this.model);
-            const executor = createSqlAgent(this.model, toolkit);
-    
-            console.log(`Executing with input "${payload.userInput}"...`);
-            const input = payload.userInput
-            const result = await executor.invoke({ input });
-    
-            console.log(`Got output ${result.output}`);
-    
-            return result */;
         } catch(err:any) {
             console.error(err.message);
             throw err;
         }
-        
-
-
-
-        
-        /* const prompt = new PromptTemplate({
-            inputVariables: ["query","result"],
-            template: template
-        });
-
-        const db_chain = new SqlDatabaseChain({
-            llm: this.model,
-            database: db,
-            verbose: true,
-            prompt: prompt
-        });
-
-        const result = await db_chain.run(payload.userInput);
-        return result; */
     }
 
     private async cleanSQLQuery(query: any) {
